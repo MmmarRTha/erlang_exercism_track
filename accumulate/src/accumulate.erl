729b8e53c3157fda8a377185b5e6f9b1b7d7468d
MmmarRTHa <m27842n@outlook.com>
2023-12-16 15:08:23 -0800

    - update: main - changed solution instead of using collections / maps. Applied recursion.

--- accumulate/src/accumulate.erl
+++ accumulate/src/accumulate.erl
@@ -1,14 +1,20 @@
 -module(accumulate).
+
 -export([accumulate/2]).
 
 %%
 %% given a fun and a list, apply fun to each list item replacing list item with fun's return value.
 %%
--spec accumulate(fun((A) -> B), list(A)) -> list(B).
-
+-spec accumulate(fun((A) -> B), [A]) -> [B].
 %% Example:
 %% Double = fun(X) -> X * 2 end,
 %% List = [1, 2, 3, 4, 5],
 %% Result = accumulate:accumulate(Double, List).
 accumulate(Fn, List) ->
-    lists:map(Fn, List).
+    accumulate(Fn, List, []).
+
+accumulate(_, [], Acc) ->
+    lists:reverse(Acc);
+accumulate(Fn, [H | T], Acc) ->
+    NewAcc = [Fn(H) | Acc],
+    accumulate(Fn, T, NewAcc).
