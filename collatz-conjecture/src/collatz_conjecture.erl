967ba6a6de5bb5541a88febf538b52e497e46842
MmmarRTHa <m27842n@outlook.com>
2023-12-19 22:36:51 -0800

    - update: main - solved collatz conjecture exercism in erlang.

---
+++ collatz-conjecture/src/collatz_conjecture.erl
@@ -0,0 +1,15 @@
+-module(collatz_conjecture).
+
+-export([steps/1]).
+
+steps(N) when N > 0 ->
+    steps(N, 0);
+steps(_) ->
+    error(badarg).
+
+steps(1, Count) ->
+    Count;
+steps(N, Count) when N rem 2 =:= 0 ->
+    steps(N div 2, Count + 1);
+steps(N, Count) ->
+    steps(N * 3 + 1, Count + 1).
